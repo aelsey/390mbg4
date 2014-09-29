dc3336ac0aff30c7d683b0fc73e5a64769a51056
aelsey <aelsey@umass.edu>
2014-09-28 20:42:56 -0400

    commented

--- src/edu/umass/cs/client/AccelReadingCache.java
+++ src/edu/umass/cs/client/AccelReadingCache.java
@@ -8,19 +8,20 @@ public class AccelReadingCache
 	
 	private int stepCount;
 	
+	//this is kind of a lower bound, it's not dynamic though...more like a noise filter
 	private static final double minimumStepThreshold = .8;
+	//this is used to provide a weight to the time between steps, 
+	//the standard deviation between steps,
+	//and threshold, which is (max + min)/2
 	private static final double exponentialWeight = .1;
 	
 	private static final int size = 1000;
+	//this isn't really needed explicitly
 	private double[] accelMagnitude;
 	private int index = 0;
 	
 	private long lastStep;
 	private long weightedStepPeriod;
-	private long weightedStepPeriodA[];
-	private long weightedStepDeviationA[];
-	private int aIndex;
-	private int bIndex;
 	private long weightedStepDeviation;
 	private double weightedThreshold;
 	private double localMin;
@@ -50,6 +51,8 @@ public class AccelReadingCache
 		return this;
 	}
 	
+	//updates max/min if the vector changes direction and re-positions 
+	//the threshold
 	public void checkPeaks(double mag){
 		if(increasing && mag <= accelMagnitude[index]){
 			this.humpMagnitude = accelMagnitude[index] - localMin;
@@ -62,15 +65,20 @@ public class AccelReadingCache
 			this.increasing = true;
 		}
 	}
-	
+	//will detect a step during calibration period and post calibration period
+	//only post calibration period will update the step counter
 	public boolean detectSteps(double mag, long now){
 		boolean updateStepInfo = false;
+		//set to true if adjustStep needs to get called
 		
+		//if the magnitude has gone below the threshold
+		//this happens once per step
+		//also, if the magnitude is greater than an arbitrary minimum step
 		if(accelMagnitude[index] >= weightedThreshold && 
 				mag <= weightedThreshold &&
 				mag > AccelReadingCache.minimumStepThreshold &&
 				this.humpMagnitude > AccelReadingCache.minimumStepThreshold)
-		{
+		{	
 			if(!isCalibrating(now) &&
 			   this.lastStep + this.weightedStepPeriod - this.weightedStepDeviation * 1.2 <= now)
 			{
@@ -88,31 +96,29 @@ public class AccelReadingCache
 		return updateStepInfo;
 	}
 	
+	//updates the metadata collected about a step
+	//step deviation: standard deviation of a step from it's expected period
+	//since the period is dynamically weighted, the deviation is too
+	
+	//step period: expected time between steps, dynamically weighted
 	public void adjustStep(long now){
+		//this just means to weight data collected during the calibration period more heavily
 		double factor = (isCalibrating(now) ? 2.5 : 1.0) * AccelReadingCache.exponentialWeight;
-		if(this.weightedStepPeriodA == null){
-			weightedStepPeriodA = new long[1000];
-			weightedStepDeviationA = new long[1000];
-		}
-		
+
 		weightedStepDeviation = 
 				(long) Math.sqrt(
 						Math.pow(now - this.lastStep - this.weightedStepPeriod, 2) * factor +
 						Math.pow(this.weightedStepDeviation, 2) * (1 - factor)
 				);
-		long w = now - lastStep;
-		long p = (long) (w * factor);
-		long d = (long) ((long) (this.weightedStepPeriod) * (1 - factor));
-		long c = p + d;
+		
 		this.weightedStepPeriod = 
 				(long) (((now - lastStep) * factor) +
 				(this.weightedStepPeriod * (1 - factor)));
 
-		weightedStepDeviationA[bIndex++] = now - this.lastStep - weightedStepPeriod;
 		this.lastStep = now;
-		weightedStepPeriodA[aIndex++] = weightedStepPeriod;
 	}
 	
+	//used to update activity view
 	public String toString(){
 		long now = System.currentTimeMillis();
 		return isCalibrating(System.currentTimeMillis()) ? 
